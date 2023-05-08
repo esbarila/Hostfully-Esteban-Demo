4c02da3171fc8c55f213653bc580cb7d5f0a41d0
esbarila <esteban.barila@gmail.com>
2023-05-08 10:48:52 -0300

    feat: first test

--- cypress/e2e/add_new_computer.cy.ts
+++ cypress/e2e/add_new_computer.cy.ts
@@ -1,7 +1,15 @@
+import { should } from "chai";
+
 describe("Adding a new computer", () => {
 
 });
 
 it("Succesfully creates a new computer", () => {
+    let computerName = (Math.random() + 1).toString(36).substring(7);
     cy.visit("/");
+    cy.get('[id=add]').click();
+    cy.get('input[id=name]').type(computerName);
+    cy.contains('Create this computer').click();
+    cy.url().should('eq', 'https://computer-database.gatling.io/computers');
+    cy.contains(`Computer ${computerName} has been created`);    
 })
